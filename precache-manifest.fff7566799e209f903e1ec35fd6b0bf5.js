3ed92421ecd0cf10261f6db76e9ed18cf371abf6
belucerrotti <bcerrotti@fi.uba.ar>
2024-02-19 22:23:24 -0300

    Updates

---
+++ precache-manifest.fff7566799e209f903e1ec35fd6b0bf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d09abc47407db2cac0e0d57eb42af740",
+    "url": "/tasks-app/index.html"
+  },
+  {
+    "revision": "2be1a34252f23a1ee957",
+    "url": "/tasks-app/static/css/main.61c1a235.chunk.css"
+  },
+  {
+    "revision": "c9388b1d3178e39c5ef1",
+    "url": "/tasks-app/static/js/2.174bc728.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/tasks-app/static/js/2.174bc728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2be1a34252f23a1ee957",
+    "url": "/tasks-app/static/js/main.71251346.chunk.js"
+  },
+  {
+    "revision": "eb35fac686e3eaf52da2",
+    "url": "/tasks-app/static/js/runtime-main.12ce5b7a.js"
+  }
+]);
